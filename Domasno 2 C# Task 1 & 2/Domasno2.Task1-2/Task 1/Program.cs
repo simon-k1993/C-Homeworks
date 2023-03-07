20f1685431666cd0f39c47be3eb6eb4c42acac9e
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2023-03-07 19:13:35 +0100

    Add files via upload

---
+++ Domasno 2 C# Task 1 & 2/Domasno2.Task1-2/Task 1/Program.cs
@@ -0,0 +1,44 @@
+﻿#region SumOfEven
+
+
+Console.WriteLine("Enter number one");
+bool isNumber1Parse = int.TryParse(Console.ReadLine(), out int number1Parsed);
+
+Console.WriteLine("Enter number two");
+bool isNumber2Parse = int.TryParse(Console.ReadLine(), out int number2Parsed);
+
+Console.WriteLine("Enter number three");
+bool isNumber3Parse = int.TryParse(Console.ReadLine(), out int number3Parsed);
+
+Console.WriteLine("Enter number four");
+bool isNumber4Parse = int.TryParse(Console.ReadLine(), out int number4Parsed);
+
+Console.WriteLine("Enter number five");
+bool isNumber5Parse = int.TryParse(Console.ReadLine(), out int number5Parsed);
+
+Console.WriteLine("Enter number six");
+bool isNumber6Parse = int.TryParse(Console.ReadLine(), out int number6Parsed);
+
+int[] arrayOfNumbers = new int[6];
+
+arrayOfNumbers[0] = number1Parsed;
+arrayOfNumbers[1] = number2Parsed;
+arrayOfNumbers[2] = number3Parsed;
+arrayOfNumbers[3] = number4Parsed;
+arrayOfNumbers[4] = number5Parsed;
+arrayOfNumbers[5] = number6Parsed;
+
+int sum = 0;
+
+foreach (int number in arrayOfNumbers)
+{
+
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+Console.WriteLine("The sum of the even numbers in the array is: " + sum);
+
+
+#endregion
---
+++ Domasno 2 C# Task 1 & 2/Domasno2.Task1-2/Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Student Group
+
+string[] studentsG1 = new string[5] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = new string[5] { "Vanco", "Pero", "Vane", "Mile", "Cacko" };
+
+Console.WriteLine("Enter 1 if you want the students from group G1, or enter 2 if you want the students from group G2!!!");
+bool number = int.TryParse(Console.ReadLine(), out int numberParsed);
+
+if (number)
+{
+    if (numberParsed == 1)
+    {
+        Console.WriteLine("The students in the group G1 are: ");
+        for (int i = 0; i < studentsG1.Length; i++)
+        {
+            Console.WriteLine(studentsG1[i]);
+        }
+
+    }
+    else if (numberParsed == 2)
+    {
+        Console.WriteLine("The students in the group G2 are: ");
+        for (int i = 0; i < studentsG2.Length; i++)
+        {
+            Console.WriteLine(studentsG2[i]);
+        }
+    }
+    else
+    {
+        Console.WriteLine("You must enter 1 or 2,no other numbers!!!");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter number 1 or number 2,everything else will result in an error!!!");
+}
+
+
+
+#endregion
+
