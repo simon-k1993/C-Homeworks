5174693c6fb21c5bd95f605675dedaed9e4d4fac
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2023-03-25 23:39:12 +0100

    Add files via upload

---
+++ Homework7-Task1-2/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿Queue<int> queue = new Queue<int>();
+
+while (true)
+{
+    Console.WriteLine("Enter a number!");
+    bool numberInput = int.TryParse(Console.ReadLine(), out int numberInputParsed);
+
+    if (numberInput)
+    {
+        queue.Enqueue(numberInputParsed);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Do you want to continue? If yes input Y/y, if no input N/n");
+        Console.ResetColor();
+        string yesOrNo = Console.ReadLine();
+
+        if (yesOrNo == "Y" || yesOrNo == "y")
+        {
+            continue;
+        }
+        else if (yesOrNo == "N" || yesOrNo == "n")
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Numbers in the queue: ");
+            Console.ResetColor();
+            foreach (int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+            break;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("You must enter Y/y or N/n!!!");
+            Console.ResetColor();
+            break;
+        }
+
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Enter a number!!!");
+        Console.ResetColor();
+        break;
+    }
+
+}
---
+++ Homework7-Task1-2/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> numbers = new List<int>();
+
+numbers.Add(3);
+numbers.Add(2);
+numbers.Add(15);
+numbers.Add(12);
+numbers.Add(9);
+numbers.Add(7);
+
+List<int> squaredNumbers = numbers.Select(x => x * x).ToList();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Squared numbers in the list: ");
+Console.ResetColor();
+foreach (int number in squaredNumbers)
+{
+    Console.WriteLine(number);
+}
