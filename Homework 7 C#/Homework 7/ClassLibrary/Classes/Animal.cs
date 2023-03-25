f9099e14d9b9727b208583c248be5524f8334a31
simon-k1993 <120113010+simon-k1993@users.noreply.github.com>
2023-03-25 23:49:15 +0100

    Add files via upload

---
+++ Homework 7 C#/Homework 7/ClassLibrary/Classes/Animal.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Classes
+{
+   public class Animal
+    {
+     
+          public Animal(string name, string color, int age, Gender gender)
+            {
+                Name = name;
+                Color = color;
+                Age = age;
+                Gender = gender;
+            }
+            public string Name { get; set; }
+            public string Color { get; set; }
+            public int Age { get; set; }
+
+            public Gender Gender { get; set; }
+        }
+    }
+
+
+
---
+++ Homework 7 C#/Homework 7/Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿using ClassLibrary.Classes;
+using ClassLibrary.Enums;
+
+List<Animal> animals = new List<Animal>();
+
+animals.Add(new Animal("Dog","brown",5,Gender.male));
+animals.Add(new Animal("Cat", "white", 2, Gender.female));
+animals.Add(new Animal("Elephant", "grey", 12, Gender.male));
+animals.Add(new Animal("Parrot", "red", 2, Gender.male));
+animals.Add(new Animal("Python", "green", 15, Gender.female));
+animals.Add(new Animal("Rabbit", "brown", 3, Gender.male));
+animals.Add(new Animal("Tiger", "Orange", 16, Gender.male));
+animals.Add(new Animal("Shrewmouse", "black", 1, Gender.male));
+animals.Add(new Animal("Chimpanzee", "brown", 5, Gender.female));
+animals.Add(new Animal("Alpaca", "brown", 3, Gender.male));
+
+List<Animal> animalsAgeFiveOrMore = animals.Where(x =>x.Age >= 5).ToList();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Animals aged 5 or older: ");
+Console.ResetColor();
+Console.WriteLine();
+foreach(Animal animal in animalsAgeFiveOrMore)
+{
+    Console.WriteLine(animal.Name);
+}
+
+Console.WriteLine();
+List<Animal> animalsWithFirstLetterA = animals.Where(x=>x.Name.StartsWith("A")).ToList();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Animals with first letter A: ");
+Console.ResetColor();
+Console.WriteLine();
+foreach(Animal animal in animalsWithFirstLetterA)
+{
+    Console.WriteLine(animal.Name);
+}
+
+Console.WriteLine();
+List<Animal> allMaleBrownAnimals = animals.Where(x=>x.Color == "brown").Where(x=>x.Gender == Gender.male).ToList();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("All male brown animals: ");
+Console.ResetColor();
+Console.WriteLine();
+foreach(Animal animal in allMaleBrownAnimals)
+{
+    Console.WriteLine(animal.Name);
+}
+
+Console.WriteLine();
+List<Animal> animalsWithNamesLongerThan10 = animals.Where(x=>x.Name.Length >= 10).ToList();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Animals with names longer than 10 characters: ");
+Console.ResetColor();
+Console.WriteLine();
+foreach(Animal animal in animalsWithNamesLongerThan10)
+{
+    Console.WriteLine(animal.Name);
+}
